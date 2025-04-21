96d6bf38cb7e3dacb351dbe20975ef01ae610a1b
Umbrason <Umbrason@Outlook.com>
2025-04-21 02:56:13 +0200

    Update BuildingPetition.cs

--- Assets/Game/Data/BuildingPetition.cs
+++ Assets/Game/Data/BuildingPetition.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 public class BuildingPetition
 {
@@ -6,6 +7,16 @@ public class BuildingPetition
     public HexPosition Position;
     public Building Building;
     public Dictionary<PlayerID, Dictionary<Resource, int>> ResourceSources;
+
+    public bool IsFinanced()
+    {
+        foreach (var (resource, cost) in Building.ConstructionCosts())
+        {
+            var provided = ResourceSources.Values.Sum(playerResources => playerResources.GetValueOrDefault(resource));
+            if (cost > provided) return false;
+        }
+        return true;
+    }
 
     public BuildingPetition(PlayerID playerID, HexPosition position, Building building, Dictionary<PlayerID, Dictionary<Resource, int>> resourceSources)
     {
