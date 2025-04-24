c2d1e07a1c4cc0fa1c76f980dad2311eca15b3f9
OlivierGuillaume <OlivierGuillaume@users.noreply.github.com>
2025-04-24 18:40:40 +0200

    fix accusation ai

--- Assets/Game/AI/Scripts/AIPhaseData.cs
+++ Assets/Game/AI/Scripts/AIPhaseData.cs
@@ -4,4 +4,5 @@ using UnityEngine;
 public abstract class AIPhaseData<T> : ScriptableObject where T : IGamePhase
 {
     public abstract IEnumerator PlayingPhase(T Phase, AIPlayer AI);
+
 }
--- Assets/Game/AI/Scripts/AccusationPhase/AIAccusationPhaseRandomData.cs
+++ Assets/Game/AI/Scripts/AccusationPhase/AIAccusationPhaseRandomData.cs
@@ -8,8 +8,15 @@ public class AIAccusationPhaseRandomData : AIAccusationPhaseData
 {
     [SerializeField, Range(0f,1f)] private float AccuseProbability = 0.01f;
     [SerializeField, Range(0f,1f)] private float AgreeProbability = 0.5f;
+
+    bool _voteStarted = false;
+    PlayerID[] _accusedPlayers = null;
+
     public override IEnumerator PlayingPhase(AccusationPhase Phase, AIPlayer AI)
     {
+        _voteStarted = false;
+        Phase.OnAccusationVoteStarted += OnAccusationVoteStarted;
+
         yield return new WaitForSeconds(AI.Config.ActionDelay);
 
         if (AccuseProbability > UnityEngine.Random.value)
@@ -34,9 +41,21 @@ public class AIAccusationPhaseRandomData : AIAccusationPhaseData
             Phase.Accuse(null);
         }
 
+        while (true)
+        {
+            yield return new WaitUntil(() => _voteStarted);
+            yield return new WaitForSeconds(AI.Config.ActionDelay);
 
-        bool vote = UnityEngine.Random.value <= AgreeProbability;
-        AI.Log("votes " + (vote ? "yes" : "no"));
-        Phase.Vote(vote);;
+            bool vote = UnityEngine.Random.value <= AgreeProbability;
+            AI.Log("votes " + (vote ? "yes" : "no"));
+            Phase.Vote(vote);
+        }
+
+    }
+
+    private void OnAccusationVoteStarted(PlayerID[] accusedPlayers)
+    {
+        _voteStarted = true;
+        _accusedPlayers = accusedPlayers;
     }
 }
