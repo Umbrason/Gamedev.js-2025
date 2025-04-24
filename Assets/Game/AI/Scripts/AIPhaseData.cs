9de70599e220010213e5a93e08d97391cbd5d312
OlivierGuillaume <OlivierGuillaume@users.noreply.github.com>
2025-04-24 18:10:20 +0200

    ai update

--- Assets/Game/AI/Scripts/AIPhaseData.cs
+++ Assets/Game/AI/Scripts/AIPhaseData.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 
 public abstract class AIPhaseData<T> : ScriptableObject where T : IGamePhase
 {
-    public abstract IEnumerator PlayingPhase(T Phase, AIConfigData Config, GameInstance GameInstance);
+    public abstract IEnumerator PlayingPhase(T Phase, AIPlayer AI);
 }
