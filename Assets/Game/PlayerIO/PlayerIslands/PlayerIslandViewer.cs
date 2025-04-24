cc4a97e99820fe34d54ba986ff7def251a88ed40
Umbrason <Umbrason@Outlook.com>
2025-04-24 13:03:24 +0200

    Faction Islands

--- Assets/Game/PlayerIO/PlayerIslands/PlayerIslandViewer.cs
+++ Assets/Game/PlayerIO/PlayerIslands/PlayerIslandViewer.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class PlayerIslandViewer : MonoBehaviour
 {
     [SerializeField] private GameInstance Game;
-    private readonly Dictionary<PlayerID, IslandView> viewInstances = new();
+    private readonly Dictionary<PlayerFaction, IslandView> viewInstances = new();
     [SerializeField] List<IslandView> viewTemplates;
 
     private void Awake()
     {
-        for (int i = 0; i < NetworkUtils.playerCount; i++)
+        for (int i = 0; i < 6; i++)
         {
-            var slot = (PlayerID)i;
+            var slot = (PlayerFaction)(i + 1);
             var instance = Instantiate(viewTemplates[i], transform);
             viewInstances[slot] = instance;
             instance.gameObject.SetActive(false);
         }
     }
 
-    void UpdateIsland(PlayerIsland island) => viewInstances[m_TargetPlayer].Data = island;
+    void UpdateIsland(PlayerIsland island) => viewInstances[Game.PlayerData[m_TargetPlayer].Faction].Data = island;
 
     private PlayerID m_TargetPlayer;
     public PlayerID TargetPlayer
@@ -30,10 +30,11 @@ public class PlayerIslandViewer : MonoBehaviour
             if (m_TargetPlayer != PlayerID.None) Game.PlayerData[m_TargetPlayer].OnIslandChanged -= UpdateIsland;
             m_TargetPlayer = value;
             if (m_TargetPlayer != PlayerID.None) Game.PlayerData[m_TargetPlayer].OnIslandChanged += UpdateIsland;
+            var playerFaction = Game.PlayerData[value].Faction;
             foreach (var (id, instance) in viewInstances)
             {
-                instance.gameObject.SetActive(id == value);
-                instance.Data = Game.PlayerData[id].Island;
+                instance.gameObject.SetActive(id == Game.PlayerData[value].Faction);
+                if (id == playerFaction) instance.Data = Game.PlayerData[value].Island;
             }
         }
     }
