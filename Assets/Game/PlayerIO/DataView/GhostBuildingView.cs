bfb3507571d60dc25d28dfb9ee0929be8850da30
OlivierGuillaume <OlivierGuillaume@users.noreply.github.com>
2025-04-26 16:03:56 +0200

    bugfix accusation + yield preview

--- Assets/Game/PlayerIO/DataView/GhostBuildingView.cs
+++ Assets/Game/PlayerIO/DataView/GhostBuildingView.cs
@@ -1,8 +1,12 @@
 using DataView;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GhostBuildingView : EnumView<Building>
 {
+    [SerializeField] StringEvent OnYieldChange;
+    [SerializeField] GameInstance Game;
+
     [SerializeField] Material GhostMaterial;
     protected override void Refresh(Building? oldData, Building? newData)
     {
@@ -10,5 +14,14 @@ public class GhostBuildingView : EnumView<Building>
         if (!_instance) return;
         var srs = _instance.GetComponentsInChildren<SpriteRenderer>();
         foreach (var sr in srs) sr.material = GhostMaterial;
+    }
+
+    private void Update()
+    {
+        if(data == null) return;
+        if(Game == null) return;
+
+        float yield = BuildingExtensions.ExpectedYield((Building)data, Game.ClientPlayerData.Island, HexOrientation.Active * transform.position);
+        OnYieldChange.Invoke(Mathf.RoundToInt(yield * 100f) + "%");
     }
 }
