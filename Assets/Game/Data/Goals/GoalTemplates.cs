9bc17247d2f0507f1a61bea449f5bfac00502d0c
heggie-dev <153226076+jakobHeggie@users.noreply.github.com>
2025-04-26 11:35:56 +0200

    Secret Task Add Boon to Description

--- Assets/Game/Data/Goals/GoalTemplates.cs
+++ Assets/Game/Data/Goals/GoalTemplates.cs
@@ -58,5 +58,5 @@ public static class GoalTemplates
         })
     };
     */
-    public static IReadOnlyList<SecretTask> IndividualSecretTasks { get; } = ((Building[])Enum.GetValues(typeof(Building))).Select(building => new BuildingAmount($"Have at least 3 {building}'s.", new() { { building, 5 } })).ToList();
+    public static IReadOnlyList<SecretTask> IndividualSecretTasks { get; } = ((Building[])Enum.GetValues(typeof(Building))).Select(building => new BuildingAmount($"Have at least 3 {building}'s.\n\nComplete this task to double all building production.", new() { { building, 5 } })).ToList();
 }
