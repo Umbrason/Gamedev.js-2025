bbcfc632c0ec9f9c2a04a29f473ef6c54a12cb56
heggie-dev <153226076+jakobHeggie@users.noreply.github.com>
2025-04-26 11:07:17 +0200

    Hide Back to Menu Button (again)

--- Assets/_Scripts/UI/PhaseCountdown.cs
+++ Assets/_Scripts/UI/PhaseCountdown.cs
@@ -11,8 +11,8 @@ public class PhaseCountdown : MonoBehaviour
     [SerializeField] private UnityEvent<string> TimeRemaining;
     [SerializeField] private Button SkipButton;
 
-    ITimedPhase _phase;
-
+    private ITimedPhase _phase;
+    private int _lastDisplayedTime = -1;
 
     private void Start()
     {
@@ -29,17 +29,41 @@ public class PhaseCountdown : MonoBehaviour
         _phase = phase as ITimedPhase;
         IsActive?.Invoke(_phase != null);
         SkipButton.gameObject.SetActive(_phase?.CanSkip() ?? false);
+        _lastDisplayedTime = -1;
     }
 
     private void Update()
     {
         if (_phase == null) return;
+
         if (GameNetworkManager.Instance.GamePausedBecauseOfDisconnect)
         {
             _phase.startTime += Time.unscaledDeltaTime;
         }
+
         Progress.Invoke(_phase.Progress);
-        TimeRemaining.Invoke(Mathf.Max(Mathf.CeilToInt(_phase.TimeRemaining), 0).ToString());
+
+        int currentTime = Mathf.Max(Mathf.CeilToInt(_phase.TimeRemaining), 0);
+
+        if (currentTime != _lastDisplayedTime)
+        {
+            _lastDisplayedTime = currentTime;
+            OnTimeChanged(currentTime);
+        }
+
+        TimeRemaining.Invoke(currentTime.ToString());
+    }
+
+    private void OnTimeChanged(int newTime)
+    {
+        if (newTime <= 5)
+        {
+            switch (gameInstance.ClientPlayerData.Role)
+            {
+                case PlayerRole.Balanced: SoundAndMusicController.Instance.PlaySFX(SFXType._25_TimerTicking_GoodVersion, gameInstance.ClientID); break;
+                case PlayerRole.Selfish: SoundAndMusicController.Instance.PlaySFX(SFXType._44_TimerTicking_SusVersion, gameInstance.ClientID); break;
+            }
+        }
     }
 
     public void SkipPhase()
